7f2f3b87e99f4e27587d6b6176db9de63c6d5794
Akshat Tiwari <akshattiwari@gmail.com>
2025-04-08 00:22:24 +0530

    login/signup updated

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const path = require("path");
 const hbs = require("hbs");
+const collection = require("./mongodb");
 
 const tempelatePath = path.join(__dirname, "../tempelates");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "../public")));
 app.set("view engine", "hbs");
-app.set("views",tempelatePath);
+app.set("views", tempelatePath);
 
 app.listen(3000, () => {
   console.log(`Server is running on http://localhost:3000/`);
@@ -19,5 +21,32 @@ app.get('/', (req, res) => {
 });
 
 app.get('/signup', (req, res) => {
-    res.render("signup");
+  res.render("signup");
+});
+
+app.post("/signup", async (req, res) => {
+  const data = {
+    email: req.body.email,
+    password: req.body.password
+  }
+
+  await collection.insertMany([data]);
+  res.render("home");
+});
+
+app.get('/login', (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const check = await collection.findOne({ email: req.body.email });
+    if (check.password === req.body.password) {
+      res.render("home");
+    } else {
+      res.send("Wrong Password");
+    }
+  } catch {
+    res.send("Wrong Details");
+  }
 });
